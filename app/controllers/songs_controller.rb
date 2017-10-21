d3dcd29be5728cd78802f89e3ac22049f48ce09e
Sarah Schaller <sarcsch@aol.com>
2017-10-21 13:29:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,10 +16,9 @@ class SongsController < ApplicationController
     @song = Song.create(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
-
     @song.save
+    
     flash[:message] = "Successfully created song."
-
     redirect to :"/songs/#{@song.slug}"
   end
 
@@ -37,15 +36,10 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-
     @song.save
-
 
     flash[:message] = "Successfully updated song."
     redirect to :"/songs/#{@song.slug}"
   end
-
-
-
-
+  
 end
