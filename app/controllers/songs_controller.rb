7e36de5723437a2264850ecf9161116232668314
Sarah Schaller <sarcsch@aol.com>
2017-10-21 13:20:34 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,7 +35,7 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.update(:name => params[:song])
+    @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(:name => params[:"Artist Name"])
 
     @song.save
